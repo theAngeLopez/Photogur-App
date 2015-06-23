0c918f6dee7c7c0697c05f53e4e06309a8acd57d
AngeLopez <angel@angelopez.ca>
2015-06-23 15:09:49 -0400

    Create route, controller and view for /pictures

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get 'pictures' => 'pictures#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
